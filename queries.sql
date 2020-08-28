d9781acef0cd4d134e334fdfc9ccdd651d5e6a68
sloth143chunk <68003372+sloth143chunk@users.noreply.github.com>
2020-08-27 17:46:37 -0700

    Updated error in titles table syntax

--- queries.sql
+++ queries.sql
@@ -1,4 +1,6 @@
-SELECT * FROM titles;
 SELECT * FROM departments;
 SELECT * FROM employees;
 SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
--- schema.sql
+++ schema.sql
@@ -51,5 +51,4 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
 );
